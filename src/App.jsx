96509638a48e6a509b6de6b15dab0139af007691
Syed Adnan <140173623+Syed-Adnan636@users.noreply.github.com>
2023-12-12 23:07:30 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import ChangeTitle from './useState/ChangeTitle'
-import Count from './count/Count'
-import ShortCircuits from './shortCircuits/ShortCircuits'
 import Birthdays from './BirthdayReminder/Birthdays'
 
 const App = () => {
   return (
     <>
-      {/* <ChangeTitle/> */}
-      {/* <Count/> */}
-      {/* <ShortCircuits/> */}
       <Birthdays/>
     </>
   )
